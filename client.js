2ba05d2a707d7c089ad7c8fc922c260409a60ca7
Muhyiddin A M Hayat <muhyiddin@unismuh.ac.id>
2023-09-04 01:58:46 +0800

    Edit

--- client.js
+++ client.js
@@ -1,78 +1,46 @@
 const qrcode = require('qrcode-terminal');
 const {phoneNumberFormatter} = require('./helpers/formatter')
 const { Client } = require('whatsapp-web.js');
-
+const client = new Client();
 const express = require('express')
-const fs = require('fs')
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const SESSION_FILE_PATH = './whatsapp-session.json';
-let sessionCfg;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-  sessionCfg = require(SESSION_FILE_PATH);
-}
-
-const client = new Client({
-  restartOnAuthFail: true,
-  puppeteer: {
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process', // <- this one doesn't works in Windows
-      '--disable-gpu'
-    ],
-  },
-  session: sessionCfg
-});
 
 const checkRegisteredNumber = async function(number) {
-  const isRegistered = await client.isRegisteredUser(number);
-  return isRegistered;
-}
+    const isRegistered = await client.isRegisteredUser(number);
+    return isRegistered;
+  }
 
 client.on('qr', qr => {
-  qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, {small: true});
 });
 
 client.on('ready', () => {
-  console.log('Client is ready!');
-});
-
-client.on('authenticated', (session) => {
-  console.log('authenticated', 'Whatsapp is authenticated!');
-  console.log('message', 'Whatsapp is authenticated!');
-  console.log('AUTHENTICATED', session);
-  sessionCfg = session;
-  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
-    if (err) {
-      console.error(err);
-    }
-  });
-});
-
-client.on('auth_failure', function(session) {
-  console.log('message', 'Auth failure, restarting...');
-});
-
-client.on('disconnected', (reason) => {
-  console.log('message', 'Whatsapp is disconnected!');
-  fs.unlinkSync(SESSION_FILE_PATH, function(err) {
-      if(err) return console.log(err);
-      console.log('Session file deleted!');
-  });
-  client.destroy();
-  client.initialize();
+    console.log('Client is ready!');
 });
 
 client.initialize()
+
+app.post('/is-registered' , async (req, res) => {
+  const number = phoneNumberFormatter(req.body.number);
+  const isRegistered = await client.isRegisteredUser(number);
+  if (!isRegistered) {
+    return res.status(422).json({
+      status: false,
+      message: 'The number is not registered'
+    });
+  }
+  else{
+    return res.status(200).json({
+      status: true,
+      message: 'The number is registered'
+    });
+
+  }
+})
 
 app.post('/send-message' , async (req, res) => {
 
@@ -102,8 +70,16 @@ app.post('/send-message' , async (req, res) => {
         });
       });
 
+      
+
 })
 
-app.listen(8080 , ()=> {
-    console.log(`Server is listening on http://localhost:8080`);
+client.on('message', msg => {
+  if (msg.body == 'ping') {
+      msg.reply('pong');
+  }
+});
+
+app.listen(5202 , ()=> {
+    console.log(`Server is listening on http://localhost:5202`);
 })
